e857a6cdd0ebfa4c061871955123bed0e6c620e1
ikinoti <isaiah.gitonga@student.moringaschool.com>
2021-08-04 19:51:15 +0300

    rename the file

---
+++ js/project.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  $("#before1").click(function () {
+    $("#after1").show();
+    $("#before1").hide();
+  });
+  $("#after1").click(function () {
+    $("#before1").show();
+    $("#after1").hide();
+  });
+
+  $("#before2").click(function () {
+    $("#after2").show();
+    $("#before2").hide();
+  });
+  $("#after2").click(function () {
+    $("#before2").show();
+    $("#after2").hide();
+  });
+
+  $("#before3").click(function () {
+    $("#after3").show();
+    $("#before3").hide();
+  });
+  $("#after3").click(function () {
+    $("#before3").show();
+    $("#after3").hide();
+  });
+  $("#before4").click(function () {
+    $("#after4").show();
+    $("#before4").hide();
+  });
+  $("#after4").click(function () {
+    $("#before4").show();
+    $("#after4").hide();
+  });
+
+  $("#before5").click(function () {
+    $("#after5").show();
+    $("#before5").hide();
+  });
+  $("#after5").click(function () {
+    $("#before5").show();
+    $("#after5").hide();
+  });
+
+  $("#before6").click(function () {
+    $("#after6").show();
+    $("#before6").hide();
+  });
+  $("#after6").click(function () {
+    $("#before6").show();
+    $("#after6").hide();
+  });
+
+  $("#before7").click(function () {
+    $("#after7").show();
+    $("#before7").hide();
+  });
+  $("#after7").click(function () {
+    $("#before7").show();
+    $("#after7").hide();
+  });
+});
